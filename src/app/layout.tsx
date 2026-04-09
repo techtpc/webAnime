12568a6c6191decda400cc8e4aeb5299eaca3766
Muhammad-Haiqal-Al-Farizi <hiakal123@gmail.com>
2026-04-09 15:34:52 +0700

    menyelesaikan fitur search bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,18 @@ export default function RootLayout({
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       </head>
       <body className={`${inter.className} bg-[#0f0f0f] text-white antialiased`}>
+        {/* 1. Tambahkan Toaster di sini */}
+        <Toaster
+          position="bottom-right"
+          toastOptions={{
+            style: {
+              background: '#222',
+              color: '#fff',
+              border: '1px solid #333',
+            },
+          }}
+        />
+
         <div className="flex min-h-screen">
           <Sidebar />
           <main className="lg:ml-64 flex-1 flex flex-col pb-10 pt-16 md:pt-0">
